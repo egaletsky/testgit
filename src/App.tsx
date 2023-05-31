6221604397a8e50441b350a9ec4710051514fcd5
egaletsky <galetsky@gmail.com>
2023-05-31 22:51:01 +0700

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloa8d.
         </p>
+        <p>lorem ipsum dolor</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Learn React
         </a>
       </header>
     </div>
