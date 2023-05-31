a079fd32600391631c943ac9c20bca10b4c7fd31
egaletsky <galetsky@gmail.com>
2023-05-31 22:59:08 +0700

    new commit Post

---
+++ src/Post/Post.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type PostPropsType = {}
+
+export const Post = (props: PostPropsType) => {
+    return (
+        <div>
+            <h2>Post</h2>
+        </div>
+    )
+}
+
