c6bcba54dc1944a32903596dcb758687e9934df6
egaletsky <galetsky@gmail.com>
2023-05-31 22:59:19 +0700

    new commit Test

---
+++ src/Test/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type TestPropsType = {}
+
+export const Test = (props: TestPropsType) => {
+    return (
+        <div>
+            <h2>Test</h2>
+        </div>
+    )
+}
+
